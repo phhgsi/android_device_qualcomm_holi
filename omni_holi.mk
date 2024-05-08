e67db9504b72144706178fe023627b72197b305a
N00BShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-05-08 16:55:27 +0530

    Update omni_holi.mk

--- omni_holi.mk
+++ omni_holi.mk
@@ -7,16 +7,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from holi device
 $(call inherit-product, device/qualcomm/holi/device.mk)
 
 PRODUCT_DEVICE := holi
-PRODUCT_NAME := omni_holi
+PRODUCT_NAME := twrp_holi
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := Holi for arm64
 PRODUCT_MANUFACTURER := qualcomm
